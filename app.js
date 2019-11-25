cba7d6b25aa87b8d5d64ed4888ffe5c78edf1c14
Kacoo <kacoo1205@qq.com>
2019-11-25 22:00:17 +0800

    删掉ImageMagick依赖

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+/**
+ * 打算改用腾讯提供的OCR接口识别图片文字
+ * 这里保留百度的接口调用
+ * 请出门右转app2.js
+ */
 var express = require('express');
 var bodyParser = require('body-parser')
 var qs = require('qs')
@@ -188,4 +193,256 @@ const dealResData = (data) => {
   // console.log('wordList:')
   // console.log(wordList)
   return wordList
+}
+
+var a = {
+  "TextDetections":
+    [{
+      "DetectedText": "蛋儿的忧桑",
+      "Confidence": 99,
+      "Polygon": [{ "X": 11, "Y": 4 }, { "X": 12, "Y": 40 }, { "X": 195, "Y": 40 }, { "X": 195, "Y": 4 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":1}}"
+    },
+    {
+      "DetectedText": "精英",
+      "Confidence": 99,
+      "Polygon": [{ "X": 20, "Y": 81 }, { "X": 20, "Y": 50 }, { "X": 87, "Y": 50 }, { "X": 87, "Y": 82 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":1}}"
+    },
+    {
+      "DetectedText": "逆袭",
+      "Confidence": 99,
+      "Polygon": [{ "X": 10, "Y": 159 }, { "X": 10, "Y": 122 }, { "X": 85, "Y": 123 }, { "X": 85, "Y": 160 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":2}}"
+    },
+    {
+      "DetectedText": "精英",
+      "Confidence": 99,
+      "Polygon": [{ "X": 21, "Y": 172 }, { "X": 21, "Y": 205 }, { "X": 87, "Y": 204 }, { "X": 87, "Y": 171 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":3}}"
+    },
+    {
+      "DetectedText": "思念像海",
+      "Confidence": 99,
+      "Polygon": [{ "X": 11, "Y": 281 }, { "X": 12, "Y": 244 }, { "X": 159, "Y": 246 }, { "X": 159, "Y": 283 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":4}}"
+    },
+    {
+      "DetectedText": "精英",
+      "Confidence": 99,
+      "Polygon": [{ "X": 20, "Y": 325 }, { "X": 21, "Y": 292 }, { "X": 87, "Y": 293 }, { "X": 86, "Y": 326 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":5}}"
+    },
+    {
+      "DetectedText": "ming",
+      "Confidence": 99,
+      "Polygon": [{ "X": 7, "Y": 406 }, { "X": 9, "Y": 369 }, { "X": 96, "Y": 373 }, { "X": 94, "Y": 410 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":6}}"
+    }, {
+      "DetectedText": "副族长",
+      "Confidence": 99,
+      "Polygon": [{ "X": 21, "Y": 413 }, { "X": 21, "Y": 449 }, { "X": 116, "Y": 449 }, { "X": 115, "Y": 413 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":7}}"
+    },
+    {
+      "DetectedText": "Foredown涨YY",
+      "Confidence": 94,
+      "Polygon": [{ "X": 9, "Y": 491 }, { "X": 9, "Y": 529 }, { "X": 276, "Y": 527 }, { "X": 276, "Y": 488 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":8}}"
+    },
+    {
+      "DetectedText": "精英",
+      "Confidence": 99,
+      "Polygon": [{ "X": 22, "Y": 538 }, { "X": 24, "Y": 570 }, { "X": 88, "Y": 566 }, { "X": 86, "Y": 533 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":8}}"
+    }, {
+      "DetectedText": "哦哟哟好厉害",
+      "Confidence": 99,
+      "Polygon": [{ "X": 10, "Y": 647 }, { "X": 10, "Y": 610 }, { "X": 231, "Y": 612 }, { "X": 231, "Y": 648 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":9}}"
+    },
+    {
+      "DetectedText": "精英",
+      "Confidence": 99,
+      "Polygon": [{ "X": 21, "Y": 689 }, { "X": 21, "Y": 657 }, { "X": 87, "Y": 659 }, { "X": 87, "Y": 690 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":9}}"
+    }, {
+      "DetectedText": "爱是吕梦卿.",
+      "Confidence": 96,
+      "Polygon": [{ "X": 10, "Y": 769 }, { "X": 10, "Y": 731 }, { "X": 198, "Y": 731 }, { "X": 198, "Y": 769 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":10}}"
+    },
+    {
+      "DetectedText": "精英",
+      "Confidence": 99,
+      "Polygon": [{ "X": 20, "Y": 812 }, { "X": 20, "Y": 780 }, { "X": 87, "Y": 780 }, { "X": 86, "Y": 812 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":10}}"
+    }, {
+      "DetectedText": "龍之天",
+      "Confidence": 91,
+      "Polygon": [{ "X": 8, "Y": 870 }, { "X": 9, "Y": 831 }, { "X": 122, "Y": 835 }, { "X": 121, "Y": 874 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":11}}"
+    },
+    {
+      "DetectedText": "精英",
+      "Confidence": 99,
+      "Polygon": [{ "X": 20, "Y": 913 }, { "X": 21, "Y": 880 }, { "X": 88, "Y": 882 }, { "X": 87, "Y": 914 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":12}}"
+    },
+    {
+      "DetectedText": "泰勒.斯威夫特",
+      "Confidence": 92,
+      "Polygon": [{ "X": 10, "Y": 992 }, { "X": 11, "Y": 956 }, { "X": 269, "Y": 956 }, { "X": 269, "Y": 992 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":13}}"
+    },
+    {
+      "DetectedText": "豪杰",
+      "Confidence": 99,
+      "Polygon": [{ "X": 20, "Y": 1035 }, { "X": 20, "Y": 1002 }, { "X": 87, "Y": 1003 }, { "X": 87, "Y": 1036 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":13}}"
+    }, {
+      "DetectedText": "宇智波带土",
+      "Confidence": 96,
+      "Polygon": [{ "X": 10, "Y": 1078 }, { "X": 10, "Y": 1114 }, { "X": 195, "Y": 1112 }, { "X": 195, "Y": 1076 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":14}}"
+    },
+    {
+      "DetectedText": "新人",
+      "Confidence": 99,
+      "Polygon": [{ "X": 21, "Y": 1157 }, { "X": 21, "Y": 1125 }, { "X": 88, "Y": 1125 }, { "X": 88, "Y": 1158 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":14}}"
+    },
+    {
+      "DetectedText": "民咕咕",
+      "Confidence": 99,
+      "Polygon": [{ "X": 12, "Y": 1197 }, { "X": 12, "Y": 1235 }, { "X": 119, "Y": 1235 }, { "X": 119, "Y": 1196 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":15}}"
+    },
+    {
+      "DetectedText": "精英",
+      "Confidence": 99,
+      "Polygon": [{ "X": 21, "Y": 1278 }, { "X": 21, "Y": 1246 }, { "X": 87, "Y": 1247 }, { "X": 87, "Y": 1278 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":16}}"
+    },
+    {
+      "DetectedText": "给大佬倒橙汁",
+      "Confidence": 99,
+      "Polygon": [{ "X": 9, "Y": 1320 }, { "X": 9, "Y": 1359 }, { "X": 231, "Y": 1358 }, { "X": 231, "Y": 1320 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":17}}"
+    },
+    {
+      "DetectedText": "豪杰",
+      "Confidence": 99,
+      "Polygon": [{ "X": 23, "Y": 1399 }, { "X": 23, "Y": 1368 }, { "X": 86, "Y": 1368 }, { "X": 86, "Y": 1400 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":17}}"
+    },
+    {
+      "DetectedText": "唔顺Q",
+      "Confidence": 99,
+      "Polygon": [{ "X": 12, "Y": 1443 }, { "X": 12, "Y": 1478 }, { "X": 113, "Y": 1477 }, { "X": 112, "Y": 1442 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":18}}"
+    },
+    {
+      "DetectedText": "族员",
+      "Confidence": 99,
+      "Polygon": [{ "X": 19, "Y": 1489 }, { "X": 20, "Y": 1522 }, { "X": 87, "Y": 1520 }, { "X": 86, "Y": 1488 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":19}}"
+    },
+    {
+      "DetectedText": "我改名了",
+      "Confidence": 99,
+      "Polygon": [{ "X": 11, "Y": 1563 }, { "X": 11, "Y": 1599 }, { "X": 154, "Y": 1599 }, { "X": 154, "Y": 1562 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":20}}"
+    },
+    {
+      "DetectedText": "精英",
+      "Confidence": 99,
+      "Polygon": [{ "X": 22, "Y": 1610 }, { "X": 22, "Y": 1639 }, { "X": 87, "Y": 1639 }, { "X": 87, "Y": 1610 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":21}}"
+    },
+    {
+      "DetectedText": "109",
+      "Confidence": 99,
+      "Polygon": [{ "X": 558, "Y": 24 }, { "X": 560, "Y": 65 }, { "X": 621, "Y": 61 }, { "X": 618, "Y": 20 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":22}}"
+    },
+    {
+      "DetectedText": "109",
+      "Confidence": 99,
+      "Polygon": [{ "X": 555, "Y": 183 }, { "X": 556, "Y": 143 }, { "X": 620, "Y": 145 }, { "X": 618, "Y": 186 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":23}}"
+    },
+    {
+      "DetectedText": "109",
+      "Confidence": 99,
+      "Polygon": [{ "X": 556, "Y": 267 }, { "X": 556, "Y": 306 }, { "X": 620, "Y": 306 }, { "X": 619, "Y": 267 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":24}}"
+    },
+    {
+      "DetectedText": "109",
+      "Confidence": 99,
+      "Polygon": [{ "X": 559, "Y": 388 }, { "X": 559, "Y": 427 }, { "X": 619, "Y": 426 }, { "X": 619, "Y": 388 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":25}}"
+    },
+    {
+      "DetectedText": "109",
+      "Confidence": 99,
+      "Polygon": [{ "X": 558, "Y": 511 }, { "X": 560, "Y": 549 }, { "X": 621, "Y": 546 }, { "X": 619, "Y": 508 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":26}}"
+    },
+    {
+      "DetectedText": "109",
+      "Confidence": 99,
+      "Polygon": [{ "X": 555, "Y": 631 }, { "X": 556, "Y": 672 }, { "X": 620, "Y": 670 }, { "X": 619, "Y": 629 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":27}}"
+    },
+    {
+      "DetectedText": "109",
+      "Confidence": 99,
+      "Polygon": [{ "X": 559, "Y": 792 }, { "X": 559, "Y": 752 }, { "X": 619, "Y": 752 }, { "X": 619, "Y": 792 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":28}}"
+    },
+    {
+      "DetectedText": "106",
+      "Confidence": 99,
+      "Polygon": [{ "X": 554, "Y": 892 }, { "X": 556, "Y": 851 }, { "X": 622, "Y": 855 }, { "X": 620, "Y": 896 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":29}}"
+    },
+    {
+      "DetectedText": "106",
+      "Confidence": 99,
+      "Polygon": [{ "X": 559, "Y": 977 }, { "X": 559, "Y": 1015 }, { "X": 620, "Y": 1014 }, { "X": 620, "Y": 977 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":30}}"
+    },
+    {
+      "DetectedText": "105",
+      "Confidence": 99,
+      "Polygon": [{ "X": 555, "Y": 1098 }, { "X": 556, "Y": 1139 }, { "X": 621, "Y": 1136 }, { "X": 620, "Y": 1096 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":31}}"
+    },
+    {
+      "DetectedText": "105",
+      "Confidence": 99,
+      "Polygon": [{ "X": 555, "Y": 1217 }, { "X": 555, "Y": 1260 }, { "X": 621, "Y": 1259 }, { "X": 621, "Y": 1216 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":32}}"
+    },
+    {
+      "DetectedText": "102",
+      "Confidence": 99,
+      "Polygon": [{ "X": 554, "Y": 1379 }, { "X": 556, "Y": 1337 }, { "X": 621, "Y": 1341 }, { "X": 618, "Y": 1382 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":33}}"
+    },
+    {
+      "DetectedText": "97",
+      "Confidence": 99,
+      "Polygon": [{ "X": 562, "Y": 1500 }, { "X": 562, "Y": 1463 }, { "X": 612, "Y": 1463 }, { "X": 612, "Y": 1500 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":34}}"
+    },
+    {
+      "DetectedText": "84",
+      "Confidence": 99,
+      "Polygon": [{ "X": 562, "Y": 1586 }, { "X": 562, "Y": 1620 }, { "X": 614, "Y": 1619 }, { "X": 614, "Y": 1585 }],
+      "AdvancedInfo": "{\"Parag\":{\"ParagNo\":35}}"
+    }],
+  "RequestId": "9eaec55c-1c5b-458b-9fea-a6e4171f2302"
 }
